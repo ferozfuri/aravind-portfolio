177ab2007c856eebe1e0aeb7ab4391586b475701
ferozfuri <51756832+ferozfuri@users.noreply.github.com>
2020-07-28 18:50:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function myFunction(){
-    var navbar=document.getElementById("navbar");
-    var y=navbar.scrollTop;
-    document.getElementById("demo").innerHTML = "Vertically: " + y + "px";
-    if(y>=20){
-        navbar.style.opacity='100';
-    }
+postToGoogle();
+
+function postToGoogle() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var messege = $("#messege").val();
+    $.ajax({
+        url : "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeZwySuiDKFZxEvgEG7kIUQo9SXr5nf52Bgn4D3nHs8lQvjEA/formResponse",
+        data : {"entry.612262960":name,"entry.2088890865":email,"entry.1353541107":messege},
+        type : "post",
+        dataType : "xml",
+        success:function(res){
+            console.log(res);
+        }
+    });
+    return false;
 }
